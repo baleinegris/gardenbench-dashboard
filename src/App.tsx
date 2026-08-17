a0c8bcb61371ffd632d1a87d22871e0934da4a9b
Oscar Heath <136947514+baleinegris@users.noreply.github.com>
2026-08-17 14:54:21 -0400

    added html paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
@@ -5,13 +6,15 @@ import About from './pages/About'
 
 function App() {
   return (
-    <>
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </>
+      <Box sx={{ flex: 1, minHeight: 0, overflow: 'auto' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Box>
+    </Box>
   )
 }
 
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,14 @@
 function About() {
   return (
-      <iframe src="/paper.html"/>
+    <iframe
+      src="/paper.html"
+      style={{
+        display: 'block',
+        width: '100%',
+        height: '100%',
+        border: 'none',
+      }}
+    />
   )
 }
 
